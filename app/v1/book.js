9d1822f469123c187b2d1e6e328b8de0cf9a74cb
1053608167 <1053608167@qq.com>
2021-07-13 16:57:52 +0800

    xiug

--- app/v1/book.js
+++ app/v1/book.js
@@ -1,9 +1,17 @@
+/*
+ * @Author: huangl
+ * @Date: 2021-07-13 08:35:51
+ * @LastEditors: huangl
+ * @LastEditTime: 2021-07-13 09:26:20
+ * @Description: file content
+ * @FilePath: \test\app\v1\book.js
+ */
 const Router = require('@koa/router')
 const router = new Router()
 router.post('/book/latest',(ctx,next) => {
     console.log(ctx.request.query.a)
     ctx.body = {
-        key: a
+        key: aaa
     }
 })
 module.exports = router
