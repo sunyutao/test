b841375d11c8ad5dd9f24425efabfabb689f0332
1053608167 <1053608167@qq.com>
2021-08-17 14:26:53 +0800

    111

--- middlewares/exception.js
+++ middlewares/exception.js
@@ -1,8 +1,16 @@
+/*
+ * @Author: huangl
+ * @Date: 2021-08-17 13:58:50
+ * @LastEditors: huangl
+ * @LastEditTime: 2021-08-17 14:26:13
+ * @Description: file content
+ * @FilePath: \test\middlewares\exception.js
+ */
 const catchError = async (cxt, next) => {
     try {
         await next()
     } catch (error) {
-        cxt.body = '报错了！'
+        cxt.body = '报错了11111111111！'
     }
 }
 module.exports = catchError
